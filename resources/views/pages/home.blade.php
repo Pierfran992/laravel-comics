0164810cdbc9ff0c38fd7fbab53e8b5c648c6e99
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-01-30 14:37:15 +0100

    Inserite le immagini del progetto;

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="{{Vite :: asset('/resources/img/favicon.ico')}}" type="image/ico">
     @vite('resources/js/app.js')
-    <title>Document</title>
+    <title>Laravel Comics</title>
 </head>
 <body>
-    <h1 class="text-danger">Hello Wolrd!</h1>
+    
 </body>
 </html>
