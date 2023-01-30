5f763fb73495c086bc261267ed9306d5c871eda5
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-01-30 17:12:03 +0100

    Completato la parte superiore del footer: - Creati i menu di navigazione e i inseriti i copyrigth;

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,118 @@
+<div class="container-fluid ms_top_footer justify-content-between">
+    <div class="container d-flex justify-content-between align-items-stretch">
+        <div class="col-7 py-4">
+            {{-- conteiner dei vari menu del footer --}}
+            <div class="ms_container_menu d-flex flex-column flex-wrap">
+                <div class="ms_slot_menu">
+                    <h4>DC COMICS</h4>
+                    <ul>
+                        <li>
+                            Characters
+                        </li>
+                        <li>
+                            Comics
+                        </li>
+                        <li>
+                            Movies
+                        </li>
+                        <li>
+                            TV
+                        </li>
+                        <li>
+                            Games
+                        </li>
+                        <li>
+                            Videos
+                        </li>
+                        <li>
+                            News
+                        </li>
+                    </ul>
+                </div>
+                <div class="ms_slot_menu">
+                    <h4>SHOP</h4>
+                    <ul>
+                        <li>
+                            Shop DC
+                        </li>
+                        <li>
+                            Shop DC Collectibles
+                        </li>
+                    </ul>
+                </div>
+                <div class="ms_slot_menu">
+                    <h4>DC</h4>
+                    <ul>
+                        <li>
+                            Terms Of Use
+                        </li>
+                        <li>
+                            Privacy policy&lpar;New&rpar;
+                        </li>
+                        <li>
+                            Ad Choices
+                        </li>
+                        <li>
+                            Advertising
+                        </li>
+                        <li>
+                            Jobs
+                        </li>
+                        <li>
+                            Subscriptions
+                        </li>
+                        <li>
+                            Talent Workshops
+                        </li>
+                        <li>
+                            CPSC Certificates
+                        </li>
+                        <li>
+                            Ratings
+                        </li>
+                        <li>
+                            Shop Help
+                        </li>
+                        <li>
+                            Contact Us
+                        </li>
+                    </ul>
+                </div>
+                <div class="ms_slot_menu">
+                    <h4>SITES</h4>
+                    <ul>
+                        <li>
+                            DC
+                        </li>
+                        <li>
+                            MAD Magazine
+                        </li>
+                        <li>
+                            DC Kids
+                        </li>
+                        <li>
+                            DC Universe
+                        </li>
+                        <li>
+                            DC Power Visa
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="ms_ctn_copyright">
+                <p class="my-0">All Site Content TM and &copy;2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved.</p>
+                <p class="my-0">
+                    <a href="#">Cookies Settings</a>
+                </p>
+            </div>
+        </div>
+        {{-- div contenente come backgroud il logo della dc --}}
+        <div class="col-5 ms_ctn_big_logo">
+            
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid ms_bottom_footer">
+
+</div>
