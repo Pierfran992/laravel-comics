cf78d1ef6267d9ba7c4ca12798dcb8dbb454b36f
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-01-30 14:54:16 +0100

    Sviluppo: -create i file footer e header in cui andar a creare il footer e l'header che verranno condivise da tutte le pagine del progetto; - creato un file main-layout in cui ho inserito la struttura principale della pagina e che verrà esteso dai file home e product;

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="shortcut icon" href="{{Vite :: asset('/resources/img/favicon.ico')}}" type="image/ico">
-    @vite('resources/js/app.js')
-    <title>Laravel Comics</title>
-</head>
-<body>
-    <h1>home page</h1>
-</body>
-</html>
+@extends('layouts.main-layout')
+
+@section('content')
+    <h1> Home page </h1>
+@endsection
--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="shortcut icon" href="{{Vite :: asset('/resources/img/favicon.ico')}}" type="image/ico">
-    @vite('resources/js/app.js')
-    <title>Laravel Comics</title>
-</head>
-<body>
-    <h1>prodotti
-    </h1>
-</body>
-</html>
+@extends('layouts.main-layout')
+
+@section('content')
+    <h1> prodotti </h1>
+@endsection
